5c07a20ec1ce43909fae91b9bf37d0907ebb7175
ravitmannava <ravitmannava@gmail.com>
2023-08-16 15:30:39 +0530

    refractor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,29 +14,27 @@ interface Props {
 function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={4}
-        spacing={5}
-      >
-        {isLoading &&
-          skeletons.map((e) => (
-            <GameCardContainer key={e}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => {
-          return (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
-            </GameCardContainer>
-          );
-        })}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding={4}
+      spacing={5}
+    >
+      {isLoading &&
+        skeletons.map((e) => (
+          <GameCardContainer key={e}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data.map((game) => {
+        return (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        );
+      })}
+    </SimpleGrid>
   );
 }
 
